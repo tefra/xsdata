716d8e1d7ac9ec272f799af3ef81d20215937c2f
Chris T <tsoulloftas@gmail.com>
2019-10-23 21:51:13 +0300

    Initialize project
    Message:
    Create a modern library that will be able to generate
    python dataclasses for xml schema definitions.
    That later can be used to serialize into xml for
    soap or xml-rpc.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+
+from setuptools import find_packages, setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+meta = dict()  # type: dict
+with open(os.path.join(here, "xsdata", "version.py"), encoding="utf-8") as f:
+    exec(f.read(), meta)
+
+if __name__ == "__main__":
+    setup(
+        packages=find_packages(),
+        version=meta["version"],
+        install_requires=["lxml"],
+        extras_require={
+            "dev": ["pre-commit", "pytest", "pytest-cov", "codecov", "tox"]
+        },
+    )

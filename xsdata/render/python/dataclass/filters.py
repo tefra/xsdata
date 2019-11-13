19eaaa6f63b2fe1aa34ae6c37c9bf3160b567b94
Chris <chris@komposta.net>
2019-11-13 21:18:18 +0200

    Dataclass renderer move field help to class docstrings

--- xsdata/render/python/dataclass/filters.py
+++ xsdata/render/python/dataclass/filters.py
@@ -1,9 +1,38 @@
+from docformatter import format_code
+
+from xsdata.models.render import Class
+
+
 def arguments(data: dict):
     def quote(value):
-        return (
-            '"{}"'.format(value.replace('"', "'"))
-            if isinstance(value, str)
-            else value
-        )
+        if isinstance(value, str) and not has_quotes(value):
+            return '"{}"'.format(value.replace('"', "'"))
+        return value
 
     return ",\n".join([f"{key}={quote(value)}" for key, value in data.items()])
+
+
+def update(data: dict, more: dict):
+    return dict(data, **more)
+
+
+def docstring(obj: Class):
+    lines = []
+    if obj.help:
+        lines.append(obj.help)
+
+    for attr in obj.attrs:
+        description = attr.help.strip() if attr.help else ""
+        lines.append(f":ivar {attr.name}: {description}".strip())
+
+    return (
+        format_code('"""\n{}\n"""'.format("\n".join(lines))) if lines else ""
+    )
+
+
+def has_quotes(string: str):
+    quote_types = ["'''", '"""', "'", '"']
+    for quote in quote_types:
+        if string.startswith(quote) and string.endswith(quote):
+            return True
+    return False

b86e1342d53e13e705032e21361931696bdf1c43
Chris <chris@komposta.net>
2020-01-14 22:04:41 +0200

    Fix defxmlschema test 0703

--- tests/fixtures/defxmlschema/chapter07/example0703.py
+++ tests/fixtures/defxmlschema/chapter07/example0703.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from dataclasses import dataclass, field
+from typing import Optional
 
 
 class System(Enum):
@@ -6,3 +8,44 @@ class System(Enum):
     :cvar US_DRESS:
     """
     US_DRESS = "US-DRESS"
+
+
+@dataclass
+class Attributes:
+    """
+    :ivar anything:
+    :ivar color:
+    :ivar dim:
+    :ivar system:
+    """
+    class Meta:
+        name = "attributes"
+
+    anything: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            name="anything",
+            type="Attribute"
+        )
+    )
+    color: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            name="color",
+            type="Attribute"
+        )
+    )
+    dim: Optional[int] = field(
+        default=None,
+        metadata=dict(
+            name="dim",
+            type="Attribute"
+        )
+    )
+    system: Optional[System] = field(
+        default=None,
+        metadata=dict(
+            name="system",
+            type="Attribute"
+        )
+    )

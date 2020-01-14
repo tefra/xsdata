04d65eacb4514e7866e8860adade394c725e16ee
Chris <chris@komposta.net>
2020-01-14 20:27:56 +0200

    Migrate completely to setup.cfg

--- setup.py
+++ setup.py
@@ -1,13 +1,3 @@
-from pathlib import Path
-
 from setuptools import setup
 
-
-def get_version(rel_path):
-    for line in Path(rel_path).read_text().splitlines():
-        if line.startswith("__version__"):
-            return line.split('"')[1]
-    raise RuntimeError("Unable to find version string.")
-
-
-setup(version=get_version("xsdata/__init__.py"))
+setup()

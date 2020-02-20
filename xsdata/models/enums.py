a4947daea7b14633482dc34ad7839153b331736a
Chris <chris@komposta.net>
2020-02-20 19:42:46 +0200

    Add xs:dateTimeStamp data type

--- xsdata/models/enums.py
+++ xsdata/models/enums.py
@@ -39,6 +39,7 @@ class DataType(Enum):
     BYTE = ("byte", int)
     DATE = ("date", str)
     DATE_TIME = ("dateTime", str)
+    DATE_TIMESTAMP = ("dateTimeStamp", str)
     DAY_TIME_DURATION = ("dayTimeDuration", str)
     YEAR_MONTH_DURATION = ("yearMonthDuration", str)
     DECIMAL = ("decimal", float)

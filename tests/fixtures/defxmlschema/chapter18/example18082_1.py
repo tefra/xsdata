f468a7065685d914be53847df6ecf3ae7a25fdbb
Chris <chris@komposta.net>
2020-03-03 18:42:28 +0200

    Improve handling with circular imports

---
+++ tests/fixtures/defxmlschema/chapter18/example18082_1.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class NewSize:
+    """
+    :ivar value:
+    """
+    class Meta:
+        name = "newSize"
+        namespace = "http://datypic.com/prod"
+
+    value: Optional[int] = field(
+        default=None,
+        metadata=dict(
+            name="value",
+            type="Extension",
+            min_inclusive=2.0,
+            max_inclusive=16.0
+        )
+    )
---
+++ tests/fixtures/defxmlschema/chapter18/example18102_1.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class ProductType:
+    """
+    :ivar number:
+    :ivar name:
+    :ivar color:
+    """
+    number: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            name="number",
+            type="Element",
+            namespace="",
+            required=True
+        )
+    )
+    name: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            name="name",
+            type="Element",
+            namespace="",
+            required=True
+        )
+    )
+    color: Optional[str] = field(
+        default=None,
+        metadata=dict(
+            name="color",
+            type="Element",
+            namespace="",
+            required=True
+        )
+    )

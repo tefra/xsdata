38df9b8756ad2ac1aa49ca0e0184c04d6077adf8
Chris <chris@komposta.net>
2020-05-11 22:54:45 +0300

    Remove duplicate namespaces from wildcard xs elements

--- tests/models/elements/test_any_attribute.py
+++ tests/models/elements/test_any_attribute.py
@@ -17,7 +17,7 @@ class AnyAttributeTests(TestCase):
         obj.namespace = "foo"
         self.assertEqual("foo", obj.raw_namespace)
 
-        obj = AnyAttribute.create(namespace="    foo  \n    \t  \r  bar")
+        obj = AnyAttribute.create(namespace="    foo  \n    \t  \r  bar foo ")
         self.assertEqual("foo bar", obj.raw_namespace)
 
     def test_property_real_name(self):
--- xsdata/utils/collections.py
+++ xsdata/utils/collections.py
@@ -1,16 +1,20 @@
 from typing import Any
 from typing import List
+from typing import Optional
 from typing import Sequence
 
 
-def unique_sequence(items: Sequence, key: str) -> List:
+def unique_sequence(items: Sequence, key: Optional[str] = None) -> List:
     seen = set()
 
     def is_new(val: Any) -> bool:
+        if key:
+            val = getattr(val, key)
+
         if val in seen:
             return False
 
         seen.add(val)
         return True
 
-    return [item for item in items if is_new(getattr(item, key))]
+    return [item for item in items if is_new(item)]

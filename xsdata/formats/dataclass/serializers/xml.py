48002cf5ef0394e09f0c75a7a7abf62a0b4dced9
Chris <chris@komposta.net>
2020-09-16 16:08:19 +0300

    XmlSerializer use XmlWriter for output generation

--- xsdata/formats/dataclass/serializers/xml.py
+++ xsdata/formats/dataclass/serializers/xml.py
@@ -1,16 +1,15 @@
 from dataclasses import dataclass
 from dataclasses import field
 from dataclasses import is_dataclass
+from io import StringIO
 from typing import Any
-from typing import Iterator
+from typing import Dict
+from typing import Generator
 from typing import List
 from typing import Optional
-from typing import Tuple
-
-from lxml.etree import cleanup_namespaces
-from lxml.etree import Element
-from lxml.etree import SubElement
-from lxml.etree import tostring
+from typing import TextIO
+from typing import Type
+from xml.etree.ElementTree import QName
 
 from xsdata.exceptions import SerializerError
 from xsdata.formats.bindings import AbstractSerializer
@@ -18,13 +17,18 @@ from xsdata.formats.dataclass.context import XmlContext
 from xsdata.formats.dataclass.models.elements import XmlMeta
 from xsdata.formats.dataclass.models.elements import XmlVar
 from xsdata.formats.dataclass.models.generics import AnyElement
-from xsdata.formats.dataclass.models.generics import Namespaces
-from xsdata.formats.dataclass.serializers.utils import SerializeUtils
+from xsdata.formats.dataclass.serializers.handlers import LxmlContentWriter
+from xsdata.formats.dataclass.serializers.mixins import XmlEventWriter
 from xsdata.models.enums import FormType
 from xsdata.models.enums import QNames
 from xsdata.utils import text
+from xsdata.utils.namespaces import prefix_exists
 
 DEFAULT_NS_PREFIX = ""
+
+
+NoneStr = Optional[str]
+EMPTY_MAP: Dict = {}
 
 
 @dataclass
@@ -42,189 +46,219 @@ class XmlSerializer(AbstractSerializer):
     encoding: str = field(default="UTF-8")
     pretty_print: bool = field(default=False)
     context: XmlContext = field(default_factory=XmlContext)
+    handler: Type[XmlEventWriter] = field(default=LxmlContentWriter)
 
-    def render(self, obj: Any, namespaces: Optional[Namespaces] = None) -> str:
+    def render(self, obj: Any, ns_map: Optional[Dict] = None) -> str:
         """
         Convert the given object tree to xml string.
 
         Optionally provide a namespaces instance with a predefined list
         of namespace uris and prefixes.
         """
-        tree = self.render_tree(obj, namespaces)
-        return tostring(
-            tree,
-            xml_declaration=self.xml_declaration,
+
+        output = StringIO()
+        self.write(output, obj, ns_map)
+        return output.getvalue()
+
+    def write(self, out: TextIO, obj: Any, ns_map: Optional[Dict] = None):
+        """Write the given object tree as xml string to the given text-io
+        output."""
+        meta = self.context.fetch(obj.__class__)
+        namespace = meta.namespace if meta.element_form == FormType.QUALIFIED else None
+
+        if ns_map is None:
+            ns_map = {}
+
+        if namespace and not prefix_exists(namespace, ns_map):
+            ns_map[None] = namespace
+
+        events = self.write_dataclass(obj)
+        handler = self.handler(
+            output=out,
+            ns_map=ns_map,
             encoding=self.encoding,
             pretty_print=self.pretty_print,
-        ).decode()
-
-    def render_tree(self, obj: Any, namespaces: Optional[Namespaces] = None) -> Element:
-        """
-        Convert a dataclass instance to a nested Element structure.
-
-        Optionally provide a namespaces instance with a predefined list
-        of namespace uris and prefixes.
-        """
-        meta = self.context.build(obj.__class__)
-        namespaces = namespaces or Namespaces()
-        namespaces.register()
-        prefix = DEFAULT_NS_PREFIX if meta.element_form == FormType.QUALIFIED else None
-        namespaces.add(meta.namespace, prefix=prefix)
-
-        root = Element(meta.qname, nsmap=namespaces.ns_map)
-        self.render_node(root, obj, namespaces)
-        cleanup_namespaces(
-            root, top_nsmap=namespaces.ns_map, keep_ns_prefixes=namespaces.prefixes
+            xml_declaration=self.xml_declaration,
         )
-        return root
+        handler.write(events)
 
-    def render_node(self, parent: Element, obj: Any, namespaces: Namespaces):
-        """Recursively traverse the given object and build the xml tree."""
-        if is_dataclass(obj):
-            self.render_complex_node(parent, obj, namespaces)
-        else:
-            SerializeUtils.set_text(parent, obj, namespaces)
+    def write_dataclass(
+        self,
+        obj: Any,
+        namespace: NoneStr = None,
+        qname: NoneStr = None,
+        nillable: bool = False,
+        xsi_type: Optional[QName] = None,
+    ) -> Generator:
+        """
+        Produce an events stream from a dataclass.
 
-    def render_complex_node(self, parent: Element, obj: Any, namespaces: Namespaces):
-        """Iterate over the dataclass fields and values and create the element
-        tree."""
+        Optionally override the qualified name and the xsi properties
+        type and nil.
+        """
 
-        parent_ns, name = text.split_qname(parent.tag)
-        meta = self.context.build(obj.__class__, parent_ns)
-        for var, value in self.next_value(meta, obj):
-            if value is None:
-                continue
-            elif var.is_attribute:
-                SerializeUtils.set_attribute(parent, var.qname, value, namespaces)
-            elif var.is_attributes:
-                SerializeUtils.set_attributes(parent, value, namespaces)
-            elif var.is_text:
-                namespaces.add(var.namespace)
-                SerializeUtils.set_text(parent, value, namespaces)
-            elif var.mixed:
-                self.render_mixed_content(parent, value, var, namespaces)
-            elif var.is_list and isinstance(value, list):
-                self.render_sub_nodes(parent, value, var, namespaces)
-            else:
-                self.render_sub_node(parent, value, var, namespaces)
-
-        SerializeUtils.set_nil_attribute(parent, meta.nillable, namespaces)
-
-    def render_mixed_content(
-        self, parent: Element, values: List, var: XmlVar, namespaces: Namespaces
-    ):
-        """Render mixed content list of values."""
-        orig_parent = parent
-        for index, value in enumerate(values):
-            if not is_dataclass(value):
-                if index == 0:
-                    SerializeUtils.set_text(parent, value, namespaces)
-                else:
-                    SerializeUtils.set_tail(parent, value, namespaces)
-            elif isinstance(value, AnyElement):
-                parent = orig_parent
-                self.render_wildcard_node(parent, value, var, namespaces)
-            else:
-                parent = orig_parent
-                self.render_element_node(parent, value, var, namespaces)
-                parent = parent[-1]
-
-    def render_sub_nodes(
-        self, parent: Element, values: List, var: XmlVar, namespaces: Namespaces
-    ):
-        """Iterate of a list of values to render the children of the given
-        parent element."""
-        for value in values:
-            self.render_sub_node(parent, value, var, namespaces)
-
-    def render_sub_node(
-        self, parent: Element, value: Any, var: XmlVar, namespaces: Namespaces
-    ):
-        """Render a child element or text content for the given parent."""
-        if isinstance(value, AnyElement):
-            self.render_wildcard_node(parent, value, var, namespaces)
-        elif var.is_element or is_dataclass(value):
-            self.render_element_node(parent, value, var, namespaces)
-        elif not parent.text:
-            SerializeUtils.set_text(parent, value, namespaces)
-        else:
-            SerializeUtils.set_tail(parent, value, namespaces)
-
-    def render_element_node(
-        self, parent: Element, value: Any, var: XmlVar, namespaces: Namespaces
-    ):
-        """Render a child element for the given parent according to the field
-        xml metadata."""
-        if hasattr(value, "qname"):
-            qname = value.qname
-        elif var.is_wildcard:
-            parent_ns, _ = text.split_qname(parent.tag)
-            meta = self.context.fetch(value.__class__, parent_ns)
-            qname = meta.qname
-        else:
-            qname = var.qname
-
+        meta = self.context.build(obj.__class__, namespace)
+        qname = qname or meta.qname
+        nillable = nillable or meta.nillable
         namespace, tag = text.split_qname(qname)
 
-        namespaces.add(namespace)
-        sub_element = SubElement(parent, qname)
-        self.render_node(sub_element, value, namespaces)
-        self.set_xsi_type(sub_element, value, var, namespaces)
-        SerializeUtils.set_nil_attribute(sub_element, var.nillable, namespaces)
+        yield XmlEventWriter.START_TAG, qname
 
-    def render_wildcard_node(
-        self, parent: Element, value: Any, var: XmlVar, namespaces: Namespaces
-    ):
-        """Render a child element for the given parent according to the
-        wildcard field metadata."""
+        for key, value in self.next_attribute(obj, meta, nillable, xsi_type):
+            yield XmlEventWriter.ADD_ATTR, key, value
+
+        for var, value in self.next_value(obj, meta):
+            if value is not None:
+                yield from self.write_value(value, var, namespace)
+
+        yield XmlEventWriter.END_TAG, qname
+
+    def write_xsi_type(self, value: Any, var: XmlVar, namespace: NoneStr) -> Generator:
+        """Produce an events stream from a dataclass with xsi abstract type
+        check."""
+        xsi_type = self.xsi_type(var, value, namespace)
+        yield from self.write_dataclass(
+            value, namespace, var.qname, var.nillable, xsi_type
+        )
+
+    def write_value(self, value: Any, var: XmlVar, namespace: NoneStr) -> Generator:
+        """
+        Delegates the given value to the correct writer according to the
+        variable metadata.
+
+        The order of the checks is important as more than one condition
+        can be true.
+        """
+        if var.is_mixed_content:
+            yield from self.write_mixed_content(value, var, namespace)
+        elif var.is_text:
+            yield from self.write_data(value, var, namespace)
+        elif var.tokens:
+            yield from self.write_tokens(value, var, namespace)
+        elif var.is_list and isinstance(value, list):
+            yield from self.write_list(value, var, namespace)
+        elif var.is_any_type:
+            yield from self.write_any_type(value, var, namespace)
+        elif var.dataclass:
+            yield from self.write_xsi_type(value, var, namespace)
+        elif var.is_element:
+            yield from self.write_element(value, var, namespace)
+        else:
+            raise SerializerError(f"Unhandled xml var: `{var.__class__.__name__}`")
+
+    def write_list(self, values: List, var: XmlVar, namespace: NoneStr) -> Generator:
+        """Produce an events stream for the given list of values."""
+        for value in values:
+            yield from self.write_value(value, var, namespace)
+
+    def write_tokens(self, value: Any, var: XmlVar, namespace: NoneStr) -> Generator:
+        """Produce an events stream for the given tokens list or list of tokens
+        lists."""
+        if value:
+            if isinstance(value[0], list):
+                for val in value:
+                    yield from self.write_element(val, var, namespace)
+            else:
+                yield from self.write_element(value, var, namespace)
+
+    def write_mixed_content(
+        self, values: List, var: XmlVar, namespace: NoneStr
+    ) -> Generator:
+        """Produce an events stream for the given list of mixed type
+        objects."""
+        for value in values:
+            yield from self.write_any_type(value, var, namespace)
+
+    def write_any_type(self, value: Any, var: XmlVar, namespace: NoneStr) -> Generator:
+        """
+        Produce an events stream for the given object.
+
+        The object can be a dataclass or a generic object or any other
+        simple type.
+        """
+        if not is_dataclass(value):
+            if var.is_element:
+                yield from self.write_element(value, var, namespace)
+            else:
+                yield from self.write_data(value, var, namespace)
+        elif isinstance(value, AnyElement):
+            yield from self.write_wildcard(value, var, namespace)
+        else:
+            qname = getattr(value, "qname", None)
+            yield from self.write_dataclass(value, namespace, qname)
+
+    def write_wildcard(
+        self, value: AnyElement, var: XmlVar, namespace: NoneStr
+    ) -> Generator:
+        """Produce an element events stream for the given generic object."""
         if value.qname:
-            sub_element = SubElement(parent, value.qname)
-        else:
-            sub_element = parent
+            namespace, tag = text.split_qname(value.qname)
+            yield XmlEventWriter.START_TAG, value.qname
 
-        namespaces.add_all(value.ns_map)
-        SerializeUtils.set_text(sub_element, value.text, namespaces)
-        SerializeUtils.set_tail(sub_element, value.tail, namespaces)
-        SerializeUtils.set_attributes(sub_element, value.attributes, namespaces)
+        for key, val in value.attributes.items():
+            yield XmlEventWriter.ADD_ATTR, key, val
+
+        yield XmlEventWriter.SET_DATA, value.text
+
         for child in value.children:
-            self.render_sub_node(sub_element, child, var, namespaces)
+            yield from self.write_any_type(child, var, namespace)
 
-        SerializeUtils.set_nil_attribute(sub_element, var.nillable, namespaces)
+        if value.qname:
+            yield XmlEventWriter.END_TAG, value.qname
 
-    def set_xsi_type(
-        self, parent: Element, value: Any, var: XmlVar, namespaces: Namespaces
-    ):
-        """Set the element's xsi:type if the given value is a derived
-        instance."""
-        if not var.clazz or value.__class__ in var.types:
-            return
+        yield XmlEventWriter.SET_DATA, value.tail
 
-        if self.context.is_derived(value, var.clazz):
-            parent_ns, _ = text.split_qname(parent.tag)
-            meta = self.context.fetch(value.__class__, parent_ns)
-            SerializeUtils.set_attribute(
-                parent, QNames.XSI_TYPE, meta.source_qname, namespaces
-            )
-        else:
-            raise SerializerError(
-                f"{value.__class__.__name__} is not derived from {var.clazz.__name__}"
-            )
+    def xsi_type(self, var: XmlVar, value: Any, namespace: NoneStr) -> Optional[QName]:
+        """Get xsi:type if the given value is a derived instance."""
+        if not value or value.__class__ in var.types or not var.dataclass:
+            return None
+
+        clazz = var.clazz
+        assert clazz is not None  # not possible :)
+
+        if self.context.is_derived(value, clazz):
+            meta = self.context.fetch(value.__class__, namespace)
+            return QName(meta.source_qname)
+
+        raise SerializerError(
+            f"{value.__class__.__name__} is not derived from {clazz.__name__}"
+        )
 
     @classmethod
-    def next_value(cls, meta: XmlMeta, obj: Any) -> Iterator[Tuple[XmlVar, Any]]:
+    def write_data(cls, value: Any, var: XmlVar, namespace: NoneStr) -> Generator:
+        """Produce a data event for the given value."""
+        yield XmlEventWriter.SET_DATA, value
+
+    @classmethod
+    def write_element(cls, value: Any, var: XmlVar, namespace: NoneStr) -> Generator:
+        """Produce an element events stream for the given simple type value."""
+        yield XmlEventWriter.START_TAG, var.qname
+
+        if var.nillable:
+            yield XmlEventWriter.ADD_ATTR, QNames.XSI_NIL, "true"
+
+        yield XmlEventWriter.SET_DATA, value
+        yield XmlEventWriter.END_TAG, var.qname
+
+    @classmethod
+    def next_value(cls, obj: Any, meta: XmlMeta):
         """
-        Return the fields and their values in the correct order according to
-        their definition and sequential metadata.
+        Return the non attribute variables with their object values in the
+        correct order according to their definition and the sequential metadata
+        property.
 
         Sequential fields need to be rendered together in parallel order
         eg: <a1/><a2/><a1/><a/2></a1>
         """
-
         index = 0
         attrs = meta.vars
         stop = len(attrs)
         while index < stop:
             var = attrs[index]
+            if var.is_attribute or var.is_attributes:
+                index += 1
+                continue
+
             if not var.sequential:
                 yield var, getattr(obj, var.name)
                 index += 1
@@ -246,3 +280,29 @@ class XmlSerializer(AbstractSerializer):
                         rolling = True
                         yield var, values[j]
                 j += 1
+
+    @classmethod
+    def next_attribute(
+        cls, obj: Any, meta: XmlMeta, xsi_nil: bool, xsi_type: Optional[QName]
+    ) -> Generator:
+        """
+        Return the attribute variables with their object values.
+
+        Ignores None values and empty lists. Optionally include the xsi
+        properties type and nil.
+        """
+        for var in meta.vars:
+            if var.is_attribute:
+                value = getattr(obj, var.name)
+                if value is None or isinstance(value, list) and not value:
+                    continue
+
+                yield var.qname, value
+            elif var.is_attributes:
+                yield from getattr(obj, var.name, EMPTY_MAP).items()
+
+        if xsi_type:
+            yield QNames.XSI_TYPE, xsi_type
+
+        if xsi_nil:
+            yield QNames.XSI_NIL, "true"

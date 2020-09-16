f95ed6615c828947f1faf83ff99f9c8f55a0bc2f
Chris <chris@komposta.net>
2020-09-16 16:11:20 +0300

    Implement lxml based event writer

---
+++ xsdata/formats/dataclass/serializers/handlers/lxml.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from dataclasses import field
+from typing import Generator
+
+from lxml.etree import tostring
+from lxml.sax import ElementTreeContentHandler
+
+from xsdata.formats.dataclass.serializers.mixins import XmlEventWriter
+
+
+@dataclass
+class LxmlContentWriter(XmlEventWriter):
+    handler: ElementTreeContentHandler = field(
+        init=False, default_factory=ElementTreeContentHandler
+    )
+
+    def write(self, events: Generator):
+        super().write(events)
+
+        self.output.write(
+            tostring(
+                self.handler.etree,
+                encoding=self.encoding,
+                pretty_print=self.pretty_print,
+                xml_declaration=self.xml_declaration,
+            ).decode()
+        )

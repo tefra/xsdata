18cff8ae015a09b7a4b9e27d36a4b3428472320c
Chris <chris@komposta.net>
2020-10-06 22:14:25 +0300

    Separate benchmarks from utils for easier profiling

--- tests/integration/benchmarks/conftest.py
+++ tests/integration/benchmarks/conftest.py
@@ -1,10 +1,88 @@
-import tempfile
-from pathlib import Path
+from tests import xsdata_temp_dir
+from tests.fixtures.books import BookForm
+from tests.fixtures.books import Books
+from xsdata.formats.dataclass.context import XmlContext
+from xsdata.formats.dataclass.parsers import XmlParser
+from xsdata.formats.dataclass.serializers import XmlSerializer
+
+xsdata_temp_dir.mkdir(parents=True, exist_ok=True)
+context = XmlContext()
 
 
 def pytest_unconfigure(config):
-    temp_xsdata = Path(tempfile.gettempdir()).joinpath("xsdata")
-    for tmp_file in temp_xsdata.glob("*"):
+    for tmp_file in xsdata_temp_dir.glob("*"):
         tmp_file.unlink()
 
-    temp_xsdata.rmdir()
+    xsdata_temp_dir.rmdir()
+
+
+def make_books(how_many: int):
+    return Books(
+        book=[
+            BookForm(
+                author="Arne Dahl",
+                title="Misterioso: A Crime Novel",
+                genre="Thrillers & Suspense",
+                price=15.95,
+                pub_date="1999-10-05",
+                review=(
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                    "Integer at erat sagittis, accumsan mauris eu, egestas "
+                    "quam. Nam tristique felis justo, vel iaculis ipsum cursus "
+                    "at. Praesent varius enim ac nunc interdum placerat. "
+                    "Integer porttitor, nibh at viverra vehicula, leo dui "
+                    "suscipit nisi, ornare laoreet eros neque nec mi. Proin."
+                ),
+                id="9788831796781",
+            )
+            for _ in range(how_many)
+        ]
+    )
+
+
+def parse(source, handler):
+    parser = XmlParser(context=context, handler=handler)
+    parser.from_bytes(source, Books)
+
+
+def write(size, obj, writer):
+    with xsdata_temp_dir.joinpath(f"benchmark_{size}.xml").open("w") as f:
+        serializer = XmlSerializer(writer=writer, context=context)
+        serializer.write(f, obj)
+
+
+if __name__ == "__main__":
+    import argparse
+    import statistics
+    from xsdata.formats.dataclass.serializers import writers
+    from xsdata.formats.dataclass.parsers import handlers
+    from timeit import Timer
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--component", default="parser", choices=["serializer", "parser"]
+    )
+    parser.add_argument("--handler", default="XmlEventHandler")
+    parser.add_argument("--number", default=1000, type=int)
+    parser.add_argument("--repeat", default=10, type=int)
+    args = parser.parse_args()
+
+    if args.component == "serializer":
+
+        writer = getattr(writers, args.handler)
+        books = make_books(args.number)
+        t = Timer(lambda: write(args.number, books, writer))
+
+    elif args.component == "parser":
+        handler = getattr(handlers, args.handler)
+        fixture = xsdata_temp_dir.joinpath(f"benchmark_{args.number}.xml")
+
+        if not fixture.exists():
+            write(args.number, make_books(args.number), writers.LxmlEventWriter)
+
+        t = Timer(lambda: parse(fixture.read_bytes(), handler))
+
+    result = t.repeat(repeat=args.repeat, number=1)
+    print("avg {}".format(statistics.mean(result)))
+    print("med {}".format(statistics.median(result)))
+    print("stdev {}".format(statistics.stdev(result)))
--- tests/integration/benchmarks/test_handlers.py
+++ tests/integration/benchmarks/test_handlers.py
@@ -3,13 +3,12 @@ import random
 import pytest
 
 from tests import xsdata_temp_dir
-from tests.fixtures.books import BookForm
-from tests.fixtures.books import Books
+from tests.integration.benchmarks.conftest import make_books
+from tests.integration.benchmarks.conftest import parse
+from tests.integration.benchmarks.conftest import write
 from xsdata.formats.dataclass.context import XmlContext
 from xsdata.formats.dataclass.parsers import handlers as readers
-from xsdata.formats.dataclass.parsers import XmlParser
 from xsdata.formats.dataclass.serializers import writers
-from xsdata.formats.dataclass.serializers import XmlSerializer
 
 context = XmlContext()
 readers_list = list(readers.__all__)
@@ -17,33 +16,6 @@ writers_list = list(writers.__all__)
 
 random.shuffle(readers_list)
 random.shuffle(writers_list)
-
-xsdata_temp_dir.mkdir(parents=True, exist_ok=True)
-
-
-def make_books(how_many: int):
-    return Books(
-        book=[
-            BookForm(
-                author="Arne Dahl",
-                title="Misterioso: A Crime Novel",
-                genre="Thrillers & Suspense",
-                price=15.95,
-                pub_date="1999-10-05",
-                review=(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
-                    "Integer at erat sagittis, accumsan mauris eu, egestas "
-                    "quam. Nam tristique felis justo, vel iaculis ipsum cursus "
-                    "at. Praesent varius enim ac nunc interdum placerat. "
-                    "Integer porttitor, nibh at viverra vehicula, leo dui "
-                    "suscipit nisi, ornare laoreet eros neque nec mi. Proin."
-                ),
-                id="9788831796781",
-            )
-            for _ in range(how_many)
-        ]
-    )
-
 
 small_books = make_books(100)
 medium_books = make_books(1000)
@@ -87,14 +59,3 @@ def test_parse_medium(benchmark, handler):
 def test_parse_large(benchmark, handler):
     src = xsdata_temp_dir.joinpath("benchmark_large.xml").read_bytes()
     benchmark(parse, src, getattr(readers, handler))
-
-
-def parse(source, handler):
-    parser = XmlParser(context=context, handler=handler)
-    parser.from_bytes(source, Books)
-
-
-def write(size, obj, writer):
-    with xsdata_temp_dir.joinpath(f"benchmark_{size}.xml").open("w") as f:
-        serializer = XmlSerializer(writer=writer)
-        serializer.write(f, obj)

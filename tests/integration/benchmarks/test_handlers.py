33eb987fb5a41ec6e69f53f0c35d662cf4fc97fb
Chris <chris@komposta.net>
2020-12-21 21:31:13 +0200

    Combine benchmarks

--- tests/integration/benchmarks/test_handlers.py
+++ tests/integration/benchmarks/test_handlers.py
@@ -17,45 +17,21 @@ writers_list = list(writers.__all__)
 random.shuffle(readers_list)
 random.shuffle(writers_list)
 
-small_books = make_books(100)
-medium_books = make_books(1000)
-large_books = make_books(10000)
+numbers = [100, 1000, 10000]
+
+fixtures = {number: make_books(number) for number in numbers}
 
 
-@pytest.mark.benchmark(disable_gc=True, group="Serialize: 100 books")
+@pytest.mark.benchmark(disable_gc=True, group="Serialize")
+@pytest.mark.parametrize("number", numbers)
 @pytest.mark.parametrize("writer", writers_list)
-def test_serialize_small(benchmark, writer):
-    benchmark(write, "small", small_books, getattr(writers, writer))
+def test_serialize(benchmark, writer, number):
+    benchmark(write, number, fixtures[number], getattr(writers, writer))
 
 
-@pytest.mark.benchmark(disable_gc=True, group="Serialize: 1000 books")
-@pytest.mark.parametrize("writer", writers_list)
-def test_serialize_medium(benchmark, writer):
-    benchmark(write, "medium", medium_books, getattr(writers, writer))
-
-
-@pytest.mark.benchmark(disable_gc=True, group="Serialize: 10000 books")
-@pytest.mark.parametrize("writer", writers_list)
-def test_serialize_large(benchmark, writer):
-    benchmark(write, "large", large_books, getattr(writers, writer))
-
-
-@pytest.mark.benchmark(disable_gc=True, group="Parse: 100 books")
+@pytest.mark.benchmark(disable_gc=True, group="Parse")
+@pytest.mark.parametrize("number", numbers)
 @pytest.mark.parametrize("handler", readers_list)
-def test_parse_small(benchmark, handler):
-    src = xsdata_temp_dir.joinpath("benchmark_small.xml").read_bytes()
-    benchmark(parse, src, getattr(readers, handler))
-
-
-@pytest.mark.benchmark(disable_gc=True, group="Parse: 1000 books")
-@pytest.mark.parametrize("handler", readers_list)
-def test_parse_medium(benchmark, handler):
-    src = xsdata_temp_dir.joinpath("benchmark_medium.xml").read_bytes()
-    benchmark(parse, src, getattr(readers, handler))
-
-
-@pytest.mark.benchmark(disable_gc=True, group="Parse: 10000 books")
-@pytest.mark.parametrize("handler", readers_list)
-def test_parse_large(benchmark, handler):
-    src = xsdata_temp_dir.joinpath("benchmark_large.xml").read_bytes()
+def test_parse(benchmark, handler, number):
+    src = xsdata_temp_dir.joinpath(f"benchmark_{number}.xml").read_bytes()
     benchmark(parse, src, getattr(readers, handler))

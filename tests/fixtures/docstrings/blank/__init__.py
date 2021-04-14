dfd5a20b155fe043f01bbd7e9937a3f4e9b4be7b
Chris <chris@komposta.net>
2021-04-14 19:39:32 +0300

    Add Blank docstrings style
    Closes #460

---
+++ tests/fixtures/docstrings/blank/__init__.py
@@ -0,0 +1,17 @@
+from tests.fixtures.docstrings.blank.schema import (
+    DoubleQuotesDescription,
+    DoubleQuotesSummary,
+    Root,
+    RootEnum,
+    RootB,
+    RootD,
+)
+
+__all__ = [
+    "DoubleQuotesDescription",
+    "DoubleQuotesSummary",
+    "Root",
+    "RootEnum",
+    "RootB",
+    "RootD",
+]
---
+++ tests/fixtures/docstrings/blank/schema.py
@@ -0,0 +1,82 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from typing import Optional
+
+__NAMESPACE__ = "urn:docs"
+
+
+@dataclass
+class DoubleQuotesDescription:
+    class Meta:
+        namespace = "urn:docs"
+
+
+@dataclass
+class DoubleQuotesSummary:
+    class Meta:
+        namespace = "urn:docs"
+
+
+class RootEnum(Enum):
+    A = "A"
+    B = "B"
+
+
+class RootB(Enum):
+    YES = "Yes"
+    NO = "No"
+
+
+class RootD(Enum):
+    TRUE_VALUE = "true"
+    FALSE_VALUE = "false"
+
+
+@dataclass
+class Root:
+    class Meta:
+        namespace = "urn:docs"
+
+    a: Optional["Root.A"] = field(
+        default=None,
+        metadata={
+            "type": "Element",
+            "namespace": "",
+            "required": True,
+        }
+    )
+    b: Optional[RootB] = field(
+        default=None,
+        metadata={
+            "type": "Element",
+            "namespace": "",
+            "required": True,
+        }
+    )
+    c: Optional[RootEnum] = field(
+        default=None,
+        metadata={
+            "type": "Element",
+            "namespace": "",
+            "required": True,
+        }
+    )
+    d: Optional[RootD] = field(
+        default=None,
+        metadata={
+            "type": "Element",
+            "namespace": "",
+            "required": True,
+        }
+    )
+
+    @dataclass
+    class A:
+        sub_a: Optional[str] = field(
+            default=None,
+            metadata={
+                "type": "Element",
+                "namespace": "",
+                "required": True,
+            }
+        )

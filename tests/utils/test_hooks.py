2dab92660606c135d4a81704caea12fea960acb8
Chris <chris@komposta.net>
2021-05-07 19:54:09 +0300

    Fix importlib metadata dict interface deprecation warning

--- tests/utils/test_hooks.py
+++ tests/utils/test_hooks.py
@@ -6,7 +6,7 @@ from xsdata.utils.hooks import load_entry_points
 
 class HooksTests(TestCase):
     @mock.patch("xsdata.utils.hooks.importlib_metadata.entry_points")
-    def test_load_entry_points(self, mock_entry_points):
+    def test_load_entry_points_with_mapping_api(self, mock_entry_points):
         first_ep = mock.Mock()
         second_ep = mock.Mock()
         third_ep = mock.Mock()
@@ -21,5 +21,16 @@ class HooksTests(TestCase):
         second_ep.load.assert_called_once_with()
         self.assertEqual(0, third_ep.load.call_count)
 
-        load_entry_points("bar")
-        third_ep.load.assert_called_once_with()
+    @mock.patch("xsdata.utils.hooks.importlib_metadata.entry_points")
+    def test_load_entry_points_with_select_api(self, mock_entry_points):
+        first_ep = mock.Mock()
+        second_ep = mock.Mock()
+
+        entry_points = mock.Mock()
+        entry_points.select.return_value = [first_ep, second_ep]
+        mock_entry_points.return_value = entry_points
+
+        load_entry_points("foo")
+        first_ep.load.assert_called_once_with()
+        second_ep.load.assert_called_once_with()
+        entry_points.select.assert_called_once_with(group="foo")

9f646daf8322c29b9da10efffdd01f7b5c528d07
Chris Tsou <chris@komposta.net>
2022-08-06 19:42:09 +0300

    Delete script.py

--- script.py
+++
@@ -1,5 +0,0 @@
-from tests.fixtures.books.fixtures import books
-from xsdata.formats.dataclass.serializers.code import PycodeSerializer
-
-serializer = PycodeSerializer()
-print(serializer.render(books))

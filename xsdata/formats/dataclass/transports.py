7583d14b7156eace59e88332b6337c49b8f0516d
Chris Tsou <chris@komposta.net>
2023-06-05 11:36:54 +0300

    Support requests sessions on the wsdl client transport (#798)

--- xsdata/formats/dataclass/transports.py
+++ xsdata/formats/dataclass/transports.py
@@ -1,8 +1,10 @@
 import abc
 from typing import Any
 from typing import Dict
+from typing import Optional
 
-import requests
+from requests import Response
+from requests import Session
 
 
 class Transport(abc.ABC):
@@ -24,27 +26,30 @@ class DefaultTransport(Transport):
     :param timeout: Read timeout
     """
 
-    __slots__ = "timeout"
+    __slots__ = "timeout", "session"
 
-    def __init__(self, timeout: float = 2.0):
+    def __init__(self, timeout: float = 2.0, session: Optional[Session] = None):
         self.timeout = timeout
+        self.session = session or Session()
 
     def get(self, url: str, params: Dict, headers: Dict) -> bytes:
         """
         :raises HTTPError: if status code is not valid for content unmarshalling.
         """
-        res = requests.get(url, params=params, headers=headers, timeout=self.timeout)
+        res = self.session.get(
+            url, params=params, headers=headers, timeout=self.timeout
+        )
         return self.handle_response(res)
 
     def post(self, url: str, data: Any, headers: Dict) -> Any:
         """
         :raises HTTPError: if status code is not valid for content unmarshalling.
         """
-        res = requests.post(url, data=data, headers=headers, timeout=self.timeout)
+        res = self.session.post(url, data=data, headers=headers, timeout=self.timeout)
         return self.handle_response(res)
 
     @classmethod
-    def handle_response(cls, response: requests.Response) -> bytes:
+    def handle_response(cls, response: Response) -> bytes:
         """
         Status codes 200 or 500 means that we can unmarshall the response.
 

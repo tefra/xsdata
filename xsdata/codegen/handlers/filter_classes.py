f89c292434b181dec68a4dea559919522c4e54b0
Christodoulos Tsoulloftas <chris@komposta.net>
2024-02-18 12:59:09 +0200

    remove: Codegen filter strategies

--- xsdata/codegen/handlers/filter_classes.py
+++ xsdata/codegen/handlers/filter_classes.py
@@ -3,7 +3,6 @@ from typing import List
 from xsdata.codegen.mixins import ContainerHandlerInterface
 from xsdata.codegen.models import Class
 from xsdata.logger import logger
-from xsdata.models.config import ClassFilterStrategy
 
 
 class FilterClasses(ContainerHandlerInterface):
@@ -15,29 +14,16 @@ class FilterClasses(ContainerHandlerInterface):
         """Main entrypoint to filter the class container.
 
         In order for a class to be considered global it has
-        to be a non-abstract element, a complex type without
-        simple content or a wsdl binding element.
+        to be derived from an element, complexType, a binding
+        operation or message
 
-        Strategies:
-            - Filter all global classes and the referenced simple types.
-            - Filter global classes with references to other global classes.
-            - Filter all classes
+        If no global types exist, all classes will be generated.
         """
-        classes = []
-        filter_strategy = self.container.config.output.filter_strategy
-        if filter_strategy == ClassFilterStrategy.ALL_GLOBALS:
-            classes = self.filter_all_globals()
-        elif filter_strategy == ClassFilterStrategy.REFERRED_GLOBALS:
-            classes = self.filter_referred_globals()
-
+        classes = self.filter_all_globals()
         if classes:
             self.container.set(classes)
-        elif filter_strategy != ClassFilterStrategy.ALL:
-            logger.warning(
-                "The filter strategy '%s' returned no classes,"
-                " will generate all types.",
-                filter_strategy.value,
-            )
+        else:
+            logger.warning("No global types exist, will generate all types.")
 
     def filter_all_globals(self) -> List[Class]:
         """Filter all globals and any referenced types.
@@ -53,24 +39,5 @@ class FilterClasses(ContainerHandlerInterface):
             if obj.is_complex_type:
                 occurs.add(obj.ref)
                 occurs.update(obj.references)
-
-        return [obj for obj in self.container if obj.ref in occurs]
-
-    def filter_referred_globals(self) -> List[Class]:
-        """Filter globals with any references.
-
-        This filter is trying to remove unused global
-        types.
-
-        Returns:
-            The list of classes for generation.
-        """
-        occurs = set()
-        for obj in self.container:
-            if obj.is_complex_type:
-                references = list(obj.references)
-                occurs.update(references)
-                if references:
-                    occurs.add(obj.ref)
 
         return [obj for obj in self.container if obj.ref in occurs]

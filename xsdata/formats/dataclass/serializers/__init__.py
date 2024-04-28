239c4fa93833773e06a07dffaf62dd2dd936babf
Christodoulos Tsoulloftas <chris@komposta.net>
2024-04-28 17:15:56 +0300

    chore: Remove tree serializers v1

--- xsdata/formats/dataclass/serializers/__init__.py
+++ xsdata/formats/dataclass/serializers/__init__.py
@@ -1,7 +1,6 @@
 from xsdata.formats.dataclass.serializers.code import PycodeSerializer
 from xsdata.formats.dataclass.serializers.dict import DictEncoder, DictFactory
 from xsdata.formats.dataclass.serializers.json import JsonSerializer
-from xsdata.formats.dataclass.serializers.tree.native import XmlTreeSerializer
 from xsdata.formats.dataclass.serializers.xml import XmlSerializer
 
 __all__ = [
@@ -11,10 +10,3 @@ __all__ = [
     "XmlSerializer",
     "PycodeSerializer",
 ]
-
-try:
-    from xsdata.formats.dataclass.serializers.tree.lxml import LxmlTreeSerializer
-
-    __all__.append("LxmlTreeSerializer")
-except ImportError:  # pragma: no cover
-    pass  # pragma: no cover
